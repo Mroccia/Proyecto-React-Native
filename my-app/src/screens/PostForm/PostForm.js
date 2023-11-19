d92a6a2b7c69b4081ece63151cdd95d76bdee54a
Lorenzo Cozzo <lcozzo@udesa.edu.ar>
2023-11-19 02:14:44 -0300

    intento de post

--- my-app/src/screens/PostForm/PostForm.js
+++ my-app/src/screens/PostForm/PostForm.js
@@ -1,77 +1,75 @@
 import React, { Component } from 'react';
-import {db, auth } from '../../firebase/config';
+import { db, auth } from '../../firebase/config';
 import Camara from '../../components/Camara/Camara';
-import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import { TextInput, TouchableOpacity, View, Text, StyleSheet, Image } from 'react-native';
 
 class PostForm extends Component {
-    constructor(){
-        super()
-        this.state={
-           owner:'',
-           textoPost:'',    
-           camera: true,
-           createdAt:'',
-           foto: ''
-        }
+    constructor() {
+        super();
+        this.state = {
+            owner: '',
+            textoPost: '',
+            camera: true,
+            createdAt: '',
+            foto: ''
+        };
     }
 
-    crearPost(){
+    crearPost() {
         db.collection('posts').add({
             owner: auth.currentUser.email,
             textoPost: this.state.textoPost,
             foto: this.state.foto,
-            likes:[],
-            comentarios:[],
+            likes: [],
+            comentarios: [],
             createdAt: Date.now()
         })
         .then(() => {
-            this.props.navigation.navigate("Home") 
+            this.props.navigation.navigate("Home");
             this.setState({
                 camera: true,
                 textoPost: ''
-            })
+            });
         })
-        .catch( e => console.log(e))
+        .catch(e => console.log(e));
     }
 
-    traerUrlDeFoto(url){
+    traerUrlDeFoto(url) {
         this.setState({
             foto: url,
             camera: false
-        })
+        });
     }
 
     render() {
-        return(
-
+        return (
             <View style={styles.formContainer}>
                 <Text style={styles.title}>New post</Text>
 
-                {this.state.camera ? 
+                {this.state.camera ? (
                     <View style={styles.camera}>
-                        <Camara traerUrlDeFoto={(url) => this.traerUrlDeFoto(url)} style={styles.camera}/> 
+                        <Camara traerUrlDeFoto={(url) => this.traerUrlDeFoto(url)} style={styles.camera} />
                     </View>
-                    : 
-                    <Image style={styles.img} source={{uri: this.state.foto}}/> 
-                }
+                ) : (
+                    <Image style={styles.img} source={{ uri: this.state.foto }} />
+                )}
 
-                <TextInput 
-                    style={styles.input} 
-                    placeholder="Escribir...    " 
-                    onChangeText={ text => this.setState({ textoPost: text }) }
+                <TextInput
+                    style={styles.input}
+                    placeholder="Escribir...    "
+                    onChangeText={text => this.setState({ textoPost: text })}
                     value={this.state.textoPost}
                 />
 
-                {this.state.foto == ''? 
-                    <Text style={styles.error}>Tenes que subir una imagen</Text>
-                    :
-                    <TouchableOpacity onPress={() => this.crearPost(auth.currentUser.owner, this.state.textoPost, this.state.foto, Date.now())}>
+                {this.state.foto === '' ? (
+                    <Text style={styles.error}>Tienes que subir una imagen</Text>
+                ) : (
+                    <TouchableOpacity onPress={() => this.crearPost(auth.currentUser.email, this.state.textoPost, this.state.foto, Date.now())}>
                         <Text style={styles.button}>Postear</Text>
                     </TouchableOpacity>
-                }
+                )}
             </View>
-
-        )
+        );
     }
 }
 
