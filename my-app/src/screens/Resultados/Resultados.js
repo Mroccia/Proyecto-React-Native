b6489d6676eafbc33ea01ac4a1001bec4b624a2e
lolo-anino <lanino@udesa.edu.ar>
2023-11-20 04:00:50 -0300

    borre todo el search y lo hice bien porque el otro no andaba

--- my-app/src/screens/Resultados/Resultados.js
+++ my-app/src/screens/Resultados/Resultados.js
@@ -1,123 +1,223 @@
-import React, {Component} from 'react';
-import { View, TextInput, Text, FlatList, TouchableOpacity, StyleSheet} from "react-native";
-import {db} from '../../firebase/config';
+import React, { Component } from "react";
+import { auth, db } from "../../firebase/config";
+import { TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList, ActivityIndicator } from "react-native";
 
+class Buscador extends Component {
+  constructor() {
+    super();
+    this.state = {
+      backup: [],
+      campoBusqueda: "",
+      filtrado: [],
+      filtrado2: [],
+      userId: "",
+      infoUsuario: null,
+      usuarios: [],
+      isLoading: false, // Nuevo estado para indicar si la búsqueda está en curso
+    };
+  }
 
-class Resultados extends Component{
-    constructor(){
-        super();
-        this.state = {
-            buscado: "", 
-            results: []
-        }
+  componentDidMount() {
+    // No necesitas usar onSnapshot aquí para obtener la lista inicial de usuarios.
+    // Puedes obtener la lista una vez y luego escuchar cambios en otro lugar si es necesario.
+    this.fetchUsuarios();
+  }
+
+  async fetchUsuarios() {
+    try {
+      const snapshot = await db.collection("users").get();
+      const usuarios = snapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data(),
+      }));
+      this.setState({ backup: usuarios });
+    } catch (error) {
+      console.error("Error al obtener usuarios:", error);
     }
+  }
 
-    buscador = () => {
-        db.collection('users').where('owner', '>=', this.state.buscado).where('owner', '<=', this.state.buscado + '\uf8ff').onSnapshot(
-            usuariosOwner => {
-        db.collection('users').where('email', '>=', this.state.buscado).where('email', '<=',this.state.buscado + '\uf8ff' ).onSnapshot(
-            (usuariosEmail => {
-                const users = []    ;
-                usuariosOwner.forEach(user => {
-                    users.push({
-                        id: user.id,
-                        datos: user.data()
-                    });
-                });
-                usuariosEmail.forEach(doc => {
-                        users.push({
-                            id: doc.id,
-                            datos: doc.data()
-                        });
-                    }
-                    
-                );
+  async busqueda() {
+    try {
+      this.setState({ isLoading: true });
 
-                let filtrado = []
+      const busquedaLowerCase = this.state.campoBusqueda.toLowerCase();
 
-                users.forEach((usuario) => {
-                    if (
-                    filtrado.some(
-                    (item) => item.id == usuario.id))  
-                        {null} 
-                    else {
-                      filtrado.push({ id: usuario.id, datos: usuario.datos });
-                    }
-                  });
-                  
-                  this.setState({
-                    results: filtrado,
-                  });
-            })
+      const filtradoUsuarios = this.state.backup.filter(elm => {
+        return elm.data.owner.toLowerCase().includes(busquedaLowerCase);
+      });
 
-    )})
-   
-}
-    
-render() {
-    console.log(this.state.results);
+      const filtradoUsuarios2 = this.state.backup.filter(elm => {
+        return elm.data.userName.toLowerCase().includes(busquedaLowerCase);
+      });
+
+      this.setState({
+        filtrado: filtradoUsuarios,
+        filtrado2: filtradoUsuarios2,
+      });
+    } catch (error) {
+      console.error("Error en la búsqueda:", error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  usuarioSeleccionado(userId) {
+    this.props.navigation.navigate("ProfileUsers", userId);
+  }
+
+  render() {
     return (
+      <View style={styles.formContainer}>
         <View style={styles.container}>
+          <View style={styles.containerBusqueda}>
             <TextInput
-                style={styles.textinput}
-                onChangeText={(text) => this.setState({ buscado: text })}
-                placeholder='Buscar usuario o email'
-                keyboardType='default'
-                value={this.state.buscado}
+              style={styles.input}
+              onChangeText={(text) => this.setState({ campoBusqueda: text })}
+              placeholder="Nombre de usuario o mail"
+              keyboardType="default"
+              value={this.state.campoBusqueda}
             />
+            <TouchableOpacity style={styles.button} onPress={() => this.busqueda()}>
+              <Text style={styles.textoBoton}>Buscar</Text>
+            </TouchableOpacity>
+          </View>
 
+          {this.state.isLoading ? (
+            <ActivityIndicator size="large" color="#0000ff" />
+          ) : this.state.filtrado.length > 0 ? (
+            <View style={styles.containerUser}>
+              <Text style={styles.textoo}>RESULTADOS DE BÚSQUEDA PARA: {this.state.campoBusqueda}</Text>
+              <Text style={styles.textoo}>MAILS:</Text>
+              <FlatList
+                data={this.state.filtrado}
+                keyExtractor={(unUsuario) => unUsuario.id}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    style={styles.botonUsuario}
+                    onPress={() => this.usuarioSeleccionado(item.id)}
+                  >
+                    <Text style={styles.usuario}>{item.data.owner}</Text>
+                  </TouchableOpacity>
+                )}
+              />
 
-            {this.state.results.length === 0 && this.state.buscado.length > 0 ?
-                <Text>Ningún usuario coincide con tu búsqueda</Text> :
-                this.state.results.length > 0 ?
-                    <FlatList
-                        data={this.state.results}
-                        keyExtractor={(user, index) => user.id + index}
-                        renderItem={({ item }) => (
-                            <View style={styles.results}>
-                                <TouchableOpacity onPress={() => this.props.navigation.navigate('Perfil', { owner: item.datos.owner })}>
-                                    <Text>{item.datos.owner} - {item.datos.email}</Text>
-                                </TouchableOpacity>
-                            </View>
-                        )}
-                    /> :
-                    <Text>Realiza una búsqueda para ver los resultados</Text>
-            }
+              <Text style={styles.textoo}>NOMBRES DE USUARIO: </Text>
+              <FlatList
+                data={this.state.filtrado2}
+                keyExtractor={(unUsuario) => unUsuario.id}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    style={styles.botonUsuario}
+                    onPress={() => this.usuarioSeleccionado(item.id)}
+                  >
+                    <Text style={styles.usuario}>{item.data.userName}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            </View>
+          ) : (
+            <View>
+              <Text>No existe el email/usuario que busca</Text>
+            </View>
+          )}
         </View>
+      </View>
     );
+  }
 }
 
-
-}
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    alignItems: 'center',  
-    justifyContent: 'center',  
-    marginTop: 10, 
-    width: '100%'
-}, 
-textinput:{
-    height:50,
-    paddingVertical:15,
-    paddingHorizontal: 10,
-    borderWidth:1,
-    borderColor: '#0099CC',
-    borderStyle: 'solid',
-    borderRadius: 6,
-    marginVertical:10,
-    width: 350
-},
-results: {
-    flex: 1, 
-    marginVertical: 10,
-    width: 350, 
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    borderRadius: 6,
-    alignItems: 'center'
-}
+    itemUsuario: {
+        color: "black",
+        padding: 20,
+        borderColor: "red",
+        borderWidth: 2
+    },
+    formContainer: {
+        paddingHorizontal: 10,
+        marginTop: 20
+    },
+    container: {
+        backgroundColor: 'rgb(255, 165, 0)',
+        borderRadius: 8,
+        height: 1000,
+        margin: 10,
+        padding: 10,
+        elevation: 5,
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent: "center",
+    },
+    containerBusqueda: {
+        flex: 1,
+        flexDirection: "row",
+        width: "100%",
+        alignItems: "center"
+    },
+    containerUser: {
+        backgroundColor: 'white',
+        borderRadius: 8,
+        margin: 10,
+        padding: 10,
+    },
+    input: {
+        height: 20,
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderStyle: "solid",
+        borderRadius: 6,
+        marginVertical: 10,
+        width: "80%",
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderWidth: 2,
+        borderColor: '#fff',
+        marginRight: 10,
+    },
+    button: {
+        backgroundColor: "green",
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: "center",
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: "solid",
+        width: "20%",
+        height: "20px",
+        paddingBottom: "15px",
+        paddingTop: " 15px",
+        flex: 1,
+        justifyContent: " center"
+    },
+    textoBoton: {
+        textAlign: "center",
+        color: "#fff",
+        width: "100%"
+    },
+    botonUsuario: {
+        backgroundColor: "rgb(40, 40, 40)",
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderColor: "grey",
+        width: "60%",
+        height: "20px",
+        paddingBottom: "15px",
+        paddingTop: " 15px",
+        flex: 1,
+        justifyContent: " center",
+        alignItems: "center"
+    },
+    textoo:{
+        marginTop: "10px",
+        marginBottom: "10px"
+    }
 })
 
-export default Resultados;
+export default Buscador;
