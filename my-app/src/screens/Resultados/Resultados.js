5e58367b492e44a4f860de8a44a6d7cd2bcf9526
lolo-anino <lanino@udesa.edu.ar>
2023-11-20 04:53:38 -0300

    ahora si resultados

--- my-app/src/screens/Resultados/Resultados.js
+++ my-app/src/screens/Resultados/Resultados.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { auth, db } from "../../firebase/config";
 import { TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList, ActivityIndicator } from "react-native";
 
-class Buscador extends Component {
+class Resultados extends Component {
   constructor() {
     super();
     this.state = {
@@ -13,56 +13,51 @@ class Buscador extends Component {
       userId: "",
       infoUsuario: null,
       usuarios: [],
-      isLoading: false, // Nuevo estado para indicar si la búsqueda está en curso
+      isLoading: false,
     };
   }
 
   componentDidMount() {
-    // No necesitas usar onSnapshot aquí para obtener la lista inicial de usuarios.
-    // Puedes obtener la lista una vez y luego escuchar cambios en otro lugar si es necesario.
     this.fetchUsuarios();
   }
 
-  async fetchUsuarios() {
-    try {
-      const snapshot = await db.collection("users").get();
-      const usuarios = snapshot.docs.map(doc => ({
-        id: doc.id,
-        data: doc.data(),
-      }));
-      this.setState({ backup: usuarios });
-    } catch (error) {
-      console.error("Error al obtener usuarios:", error);
-    }
+  fetchUsuarios() {
+    db.collection("users")
+      .get()
+      .then((snapshot) => {
+        const usuarios = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }));
+        this.setState({ backup: usuarios });
+      })
+      .catch((error) => {
+        console.error("Error al obtener usuarios:", error);
+      });
   }
 
-  async busqueda() {
-    try {
-      this.setState({ isLoading: true });
+  busqueda() {
+    this.setState({ isLoading: true });
 
-      const busquedaLowerCase = this.state.campoBusqueda.toLowerCase();
+    const busquedaLowerCase = this.state.campoBusqueda.toLowerCase();
 
-      const filtradoUsuarios = this.state.backup.filter(elm => {
-        return elm.data.owner.toLowerCase().includes(busquedaLowerCase);
-      });
+    const filtradoUsuarios = this.state.backup.filter((elm) => {
+      return elm.data.owner.toLowerCase().includes(busquedaLowerCase);
+    });
 
-      const filtradoUsuarios2 = this.state.backup.filter(elm => {
-        return elm.data.userName.toLowerCase().includes(busquedaLowerCase);
-      });
+    const filtradoUsuarios2 = this.state.backup.filter((elm) => {
+      return elm.data.userName.toLowerCase().includes(busquedaLowerCase);
+    });
 
-      this.setState({
-        filtrado: filtradoUsuarios,
-        filtrado2: filtradoUsuarios2,
-      });
-    } catch (error) {
-      console.error("Error en la búsqueda:", error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
+    this.setState({
+      filtrado: filtradoUsuarios,
+      filtrado2: filtradoUsuarios2,
+      isLoading: false,
+    });
   }
 
   usuarioSeleccionado(userId) {
-    this.props.navigation.navigate("ProfileUsers", userId);
+    this.props.navigation.navigate("OtroPerfil", userId);
   }
 
   render() {
@@ -83,17 +78,17 @@ class Buscador extends Component {
           </View>
 
           {this.state.isLoading ? (
-            <ActivityIndicator size="large" color="#0000ff" />
+            <ActivityIndicator />
           ) : this.state.filtrado.length > 0 ? (
             <View style={styles.containerUser}>
-              <Text style={styles.textoo}>RESULTADOS DE BÚSQUEDA PARA: {this.state.campoBusqueda}</Text>
-              <Text style={styles.textoo}>MAILS:</Text>
+              <Text style={styles.txt}>Resultados finales: {this.state.campoBusqueda}</Text>
+              <Text style={styles.txt}>Mail de usuario buscado:</Text>
               <FlatList
                 data={this.state.filtrado}
                 keyExtractor={(unUsuario) => unUsuario.id}
                 renderItem={({ item }) => (
                   <TouchableOpacity
-                    style={styles.botonUsuario}
+                    style={styles.buttonUsuario}
                     onPress={() => this.usuarioSeleccionado(item.id)}
                   >
                     <Text style={styles.usuario}>{item.data.owner}</Text>
@@ -101,18 +96,16 @@ class Buscador extends Component {
                 )}
               />
 
-              <Text style={styles.textoo}>NOMBRES DE USUARIO: </Text>
+              <Text style={styles.textoo}>Nombre de usuario buscado: </Text>
               <FlatList
                 data={this.state.filtrado2}
                 keyExtractor={(unUsuario) => unUsuario.id}
                 renderItem={({ item }) => (
                   <TouchableOpacity
                     style={styles.botonUsuario}
-                    onPress={() => this.usuarioSeleccionado(item.id)}
-                  >
+                    onPress={() => this.usuarioSeleccionado(item.id)}>
                     <Text style={styles.usuario}>{item.data.userName}</Text>
-                  </TouchableOpacity>
-                )}
+                  </TouchableOpacity>)}
               />
             </View>
           ) : (
@@ -127,97 +120,72 @@ class Buscador extends Component {
 }
 
 const styles = StyleSheet.create({
-    itemUsuario: {
-        color: "black",
-        padding: 20,
-        borderColor: "red",
-        borderWidth: 2
-    },
-    formContainer: {
-        paddingHorizontal: 10,
-        marginTop: 20
-    },
-    container: {
-        backgroundColor: 'rgb(255, 165, 0)',
-        borderRadius: 8,
-        height: 1000,
-        margin: 10,
-        padding: 10,
-        elevation: 5,
-        flex: 1,
-        alignItems: "flex-start",
-        justifyContent: "center",
-    },
-    containerBusqueda: {
-        flex: 1,
-        flexDirection: "row",
-        width: "100%",
-        alignItems: "center"
-    },
-    containerUser: {
-        backgroundColor: 'white',
-        borderRadius: 8,
-        margin: 10,
-        padding: 10,
-    },
-    input: {
-        height: 20,
-        paddingVertical: 15,
-        paddingHorizontal: 10,
-        borderWidth: 1,
-        borderColor: "#ccc",
-        borderStyle: "solid",
-        borderRadius: 6,
-        marginVertical: 10,
-        width: "80%",
-    },
-    image: {
-        width: 50,
-        height: 50,
-        borderWidth: 2,
-        borderColor: '#fff',
-        marginRight: 10,
-    },
-    button: {
-        backgroundColor: "green",
-        paddingHorizontal: 10,
-        paddingVertical: 6,
-        textAlign: "center",
-        borderRadius: 4,
-        borderWidth: 1,
-        borderStyle: "solid",
-        width: "20%",
-        height: "20px",
-        paddingBottom: "15px",
-        paddingTop: " 15px",
-        flex: 1,
-        justifyContent: " center"
-    },
-    textoBoton: {
-        textAlign: "center",
-        color: "#fff",
-        width: "100%"
-    },
-    botonUsuario: {
-        backgroundColor: "rgb(40, 40, 40)",
-        paddingHorizontal: 10,
-        paddingVertical: 6,
-        borderRadius: 4,
-        borderWidth: 1,
-        borderStyle: "solid",
-        borderColor: "grey",
-        width: "60%",
-        height: "20px",
-        paddingBottom: "15px",
-        paddingTop: " 15px",
-        flex: 1,
-        justifyContent: " center",
-        alignItems: "center"
-    },
-    textoo:{
-        marginTop: "10px",
-        marginBottom: "10px"
-    }
-})
+  container: {
+    backgroundColor: 'rgb(255, 165, 0)',
+    borderRadius: 8,
+    margin: 10,
+    padding: 10,
+    elevation: 5,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
 
-export default Buscador;
+  containerBusqueda: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  containerUser: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+    marginTop: 10,
+    width: 750,
+  },
+
+  input: {
+    height: 40,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderStyle: 'solid',
+    borderRadius: 6,
+    marginVertical: 10,
+    width: '70%',
+    marginRight: 10,
+  },
+
+  button: {
+    backgroundColor: 'green',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    textAlign: 'center',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    width: '30%',
+  },
+
+  buttonUsuario: {
+    backgroundColor: 'rgb(40, 40, 40)',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'grey',
+    width: '80%',
+    marginVertical: 5,
+  },
+
+  txt: {
+    marginTop: 10,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+});
+
+export default Resultados;
